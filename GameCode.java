b882c9ce97a5c41e4784fe3c6b5e4d7f310de082
korchevalex <alexkorchev123456@gmail.com>
2023-01-21 12:36:14 +0200

    Added several changes
    Added scoring system
    Fixed capital letters
    Added some punctuation

--- GameCode.java
+++ GameCode.java
@@ -2,11 +2,13 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GameCode {
-    private static final String ROCK = "Rock";
-    private static final String PAPER = "Paper";
-    private static final String SCISSORS = "Scissors";
+    private static final String ROCK = "rock";
+    private static final String PAPER = "paper";
+    private static final String SCISSORS = "scissors";
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int playerScore = 0;
+        int computerScore = 0;
 
         while(true) {
             System.out.print("Choose [r]ock, [p]aper or [s]cissors: ");
@@ -29,15 +31,19 @@ public class GameCode {
                 case 1 -> computerMove = PAPER;
                 case 2 -> computerMove = SCISSORS;
             }
-            System.out.println("The computer chose " + computerMove);
+            System.out.println("The computer chose " + computerMove + ".");
 
             if (computerMove.equals(playerMove)) {
-                System.out.println("Draw");
+                System.out.println("Draw!");
             } else if ((playerMove.equals(ROCK) && computerMove.equals(SCISSORS)) || (playerMove.equals(PAPER) && computerMove.equals(ROCK)) || (playerMove.equals(SCISSORS) && computerMove.equals(PAPER))) {
                 System.out.println("You win!");
+                playerScore++;
             } else {
-                System.out.println("You lose");
+                System.out.println("You lose!");
+                computerScore++;
             }
+            System.out.println("Player score: " + playerScore);
+            System.out.println("Computer score: " + computerScore);
 
             while (true) {
                 System.out.println("Play again?[y]es/[n]o");
